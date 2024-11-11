5aa3cdddd60e39c09e8939e0d494a987ae83302b
Gym Ikirenga <gymikirenga@Ikirengas-iMac.local>
2024-11-11 14:24:06 +0200

    some changes on udacity

--- udacity.js
+++ udacity.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var numbers = [
     [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
     [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
@@ -24,20 +24,3 @@ for (var i = 0 ; i < numbers.length ; i++){
 }
 
 console.log(numbers);
-=======
-var bills = [50.23, 19.12, 34.01,
-    100.11, 12.15, 9.90, 29.11, 12.99,
-    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
-];
-
-let total = 0 ;
-let tax = 0 ;
-
-let new_array = bills.map((bill)=>{
-    tax = bill * 0.15;
-    total = bill + tax ;
-    return total ;
-})
-
-console.log(new_array);
->>>>>>> 55428c6 (old changes)
