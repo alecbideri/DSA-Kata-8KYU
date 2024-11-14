2db70dc66b2b51ebb1a98c96cf8e4332ff4ca9aa
Gym Ikirenga <gymikirenga@Ikirengas-iMac.local>
2024-11-14 12:04:18 +0200

    new test

---
+++ test.js
@@ -0,0 +1,11 @@
+const lovePhrases = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all"
+  ];
+   
+  let n = 7 ;
+  console.log(lovePhrases[0]);
